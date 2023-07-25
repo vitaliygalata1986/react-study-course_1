2f59382c6c8c3b96ce9efde532b4658b9572fd39
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-07-25 23:43:32 +0300

    use jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
+const SomeComponent = ({ children }) => {
+  return <div>{children}</div>;
+};
+
 function App() {
-  return <div className="App"></div>;
+  let a = 100;
+  return (
+    <div className="App">
+      {a ? <h1>a {100 + 100}</h1> : 10}
+      {/* <p>sss</p> */}
+      <SomeComponent>Text</SomeComponent>
+    </div>
+  );
 }
 
 export default App;
