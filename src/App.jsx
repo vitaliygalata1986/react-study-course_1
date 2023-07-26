604d6d9d4701d057fc31419f9e36ba5187a9b651
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-07-26 15:13:40 +0300

    updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 import Book from './components/Book';
 
-function App(name, year, price) {
+function App() {
   return (
     <div>
       <Book name="JS" year="2023" price="10000" />
-      <Book name="'PHP'" year="2022" price="15000" />
+      <Book name="PHP" year="2022" price="15000" />
       <Book year="2021" price="13000" />
     </div>
   );
