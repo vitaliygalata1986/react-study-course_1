6ce9386ab6e8d9bcefa2ed8998db5555aefef354
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-07-28 15:44:33 +0300

    Создание классового компонента

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor(props) {
+    super(props); // если пропсы будут, то передадим в родительский клас
+  }
+
+  render() {
+    return <div className="App">dddd</div>;
+  }
 }
 
 export default App;
