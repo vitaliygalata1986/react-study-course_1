b1f5e0cfee931065d684650efd2c65c45d8ef144
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-01 00:36:49 +0300

    Жизненный цикл React

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {
     count: 0,
+    posts: [],
+    loading: true,
+    comments: [],
   };
 
   handleClick = (params) => {
@@ -12,7 +15,30 @@ class App extends Component {
       : this.setState({ count: this.state.count - 1 });
   };
 
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data, loading: false }));
+
+    this.timerId = setInterval(() => {
+      fetch('https://jsonplaceholder.typicode.com/comments?_start=0&_limit=2')
+        .then((response) => response.json())
+        .then((data) => this.setState({ comments: data }));
+    }, 3000);
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    // размонитируем приложение - сделаем отмену таймера
+    clearInterval(this.timerId);
+  }
+
   render() {
+    console.log('render', this.state.count);
     return (
       <div className="App">
         React
@@ -22,6 +48,14 @@ class App extends Component {
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           +
         </button>
+        <button className="btn" onClick={() => this.componentWillUnmount()}>
+          Отмена интервала
+        </button>
+        {this.state.loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <h3>{this.state.posts.length} was loaded</h3>
+        )}
       </div>
     );
   }
