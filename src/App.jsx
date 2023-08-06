319a31653ad7f3c78edd5e594ebccb5b086802f5
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-06 22:42:28 +0300

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import './App.css';
 import React, { Component } from 'react';
 
-let count = localStorage.getItem('timer') ? +localStorage.getItem('timer') : 0;
-
 class App extends Component {
   state = {
-    count,
+    count: 0,
+    isCounting: false,
   };
 
-  startTimer = (params) => {
-    console.log(this.state.count);
-    this.timerId = setInterval(() => {
+  startTimer = () => {
+    this.setState({ isCounting: true });
+
+    this.counterID = setInterval(() => {
       this.setState({ count: this.state.count + 1 });
     }, 1000);
   };
+  stopTimer = () => {};
+  resetTimer = () => {};
 
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-    localStorage.setItem('timer', this.state.count);
-  }
+  componentDidMount() {}
+
+  componentDidUpdate() {}
+
+  componentWillUnmount() {}
 
   render() {
-    localStorage.setItem('timer', this.state.count);
     return (
       <div className="App">
-        <button className="btn" onClick={() => this.componentWillUnmount()}>
-          Stop
-        </button>
-        {this.state.count}
-        <button onClick={this.startTimer}>Start</button>
-        <button onClick={() => this.setState({ count: 0 })}>Reset</button>
+        <h1>React</h1>
+        <h3>{this.state.count}</h3>
+        {!this.state.isCounting ? (
+          <button onClick={this.startTimer}>Start</button>
+        ) : (
+          <button onClick={this.stopTimer}>Stop</button>
+        )}
+        <button onClick={this.resetTimer}>Reset</button>
       </div>
     );
   }
