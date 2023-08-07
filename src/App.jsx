16660d0676102b07bc79c7f1b2a38a013ca89dd8
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-07 15:12:16 +0300

    use map

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,19 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    count: 0,
-    isCounting: false,
+    posts: [
+      { id: 'abc1', name: 'JS Basics' },
+      { id: 'abc2', name: 'JS Advanced' },
+      { id: 'abc3', name: 'React JS' },
+    ],
   };
-
-  startTimer = () => {
-    this.setState({ isCounting: true });
-
-    this.counterID = setInterval(() => {
-      this.setState({ count: this.state.count + 1 });
-    }, 1000);
-  };
-
-  stopTimer = () => {
-    this.setState({ isCounting: false });
-    clearInterval(this.counterID);
-  };
-
-  resetTimer = () => {
-    this.setState({ isCounting: false, count: 0 });
-    clearInterval(this.counterID);
-  };
-
-  componentDidUpdate() {
-    localStorage.setItem('timer', this.state.count);
-  }
-
-  componentDidMount() {
-    // один раз, как мы смонтировали приложение, нам нужно вытащить значение из localStorage
-    const userCount = localStorage.getItem('timer');
-    if (userCount) {
-      this.setState({ count: +userCount });
-    }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
 
   render() {
     return (
       <div className="App">
-        <h1>React</h1>
-        <h3>{this.state.count}</h3>
-        {!this.state.isCounting ? (
-          <button onClick={this.startTimer}>Start</button>
-        ) : (
-          <button onClick={this.stopTimer}>Stop</button>
-        )}
-        <button onClick={this.resetTimer}>Reset</button>
+        {this.state.posts.map((post, index) => (
+          <h2 key={post.id}>{post.name}</h2>
+        ))}
       </div>
     );
   }
